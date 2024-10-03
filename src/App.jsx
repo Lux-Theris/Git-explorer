bbfffc2b823541154eac20021b353d9cfa98e322
Felipe S.C. <felipe-spazziani-colliaso@outlook.com>
2024-10-02 21:10:46 -0300

    Continuação da aula, mexendo no CSS e criando a função para acessar a API do github

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Dashboard } from "./pages/Dashboard"
 import { Repositories } from "./pages/Repositories"
 import GlobalStyles from "./styles/GlobalStyles"
-
+import { ThemeProvider } from "styled-components"
+import dark from './styles/themes/dark'
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={dark}>
     <GlobalStyles />
     <Dashboard />
     <Repositories />
-    </>
+    </ThemeProvider>
   )
 }
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,16 +1,43 @@
 import { FaSearch } from "react-icons/fa";
-
 import { Container, Content} from "./styles";
+import { useState } from "react";
+import { getUserDetails } from "../../api/github";
 
 export const Dashboard = () => {
+  const [username, setUserName] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  function handleSetUser(event){
+    setUserName(event.target.value);
+  }
+
+  async function handleGetDetails(){
+    try{
+      setLoading(true);
+      const result = await getUserDetails(username);
+
+      console.log(result);
+    } catch(error) {
+      console.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <Container>
       <Content>
-        <h1>Dashboard</h1>
-
-        <input type="text" placeholder="Insert Here" />
-
-        <button>Buscar <FaSearch /></button>
+        <label htmlFor="nickname">
+          Digite o usuário GIT
+        <input 
+          id="nickname" 
+          type="text" 
+          placeholder="Insert GIT nickname here" 
+          onChange={handleSetUser}  
+        />
+        {username}
+        </label>
+        <button onClick={handleGetDetails}>Buscar <FaSearch /></button>
       </Content>
     </Container>
   )
